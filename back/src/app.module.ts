5346a691eee4362efab5bc3284e8cda9baed6579
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2023-01-16 18:42:33 -0300

    add crud questions

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+<<<<<<< Updated upstream
 import { InfraModule } from './database/infra.module';
 
 @Module({
   imports: [InfraModule],
   controllers: [],
   providers: [],
+=======
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { QuestionsModule } from './questions/questions.module';
+
+@Module({
+  imports: [QuestionsModule],
+  controllers: [AppController],
+  providers: [AppService],
+>>>>>>> Stashed changes
 })
 export class AppModule {}

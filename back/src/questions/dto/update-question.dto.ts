5857fad6d835daac6e6f0f806586720b0c363960
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2023-01-17 14:47:51 -0300

    update crud questions

--- back/src/questions/dto/update-question.dto.ts
+++ back/src/questions/dto/update-question.dto.ts
@@ -1,4 +1,53 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateQuestionDto } from './create-question.dto';
+import { ObjectId } from 'bson';
+import { Type } from 'class-transformer';
+import {
+  IsString,
+  IsOptional,
+  IsInt,
+  IsBoolean,
+  ValidateNested,
+  IsArray,
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsObject,
+} from 'class-validator';
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {}
+export class UpdateQuestionDto extends CreateQuestionDto {
+  @IsOptional()
+  @IsObject()
+  _id: ObjectId;
+
+  @IsOptional()
+  @IsString()
+  text: string;
+
+  @IsOptional()
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @IsInt()
+  level: number;
+
+  @IsOptional()
+  @IsString()
+  hint: string;
+
+  @ArrayMinSize(4)
+  @ArrayMaxSize(4)
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Answers)
+  answers: Answers[];
+}
+
+export class Answers {
+  @IsOptional()
+  @IsString()
+  text: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isCorrect: boolean;
+}
--- back/src/questions/entities/question.entity.ts
+++ back/src/questions/entities/question.entity.ts
@@ -1,1 +1,15 @@
-export class Question {}
+import { ObjectId } from 'bson';
+
+export class QuestionEntity {
+  constructor(
+    public _id: ObjectId,
+    public text: string,
+    public category: string,
+    public level: number,
+    public hint: string,
+    public answers: {
+      text: string;
+      isCorrect: boolean;
+    }[],
+  ) {}
+}

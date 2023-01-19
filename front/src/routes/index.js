fa76140eefd4be147544acb516c07378d8838829
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2023-01-19 15:58:09 -0300

    refactoring login page

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -1,9 +1,9 @@
-import { Fragment } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import useAuth from '../hooks/useAuth';
-import Home from '../pages/Home';
-
-import Signup from '../pages/Signup';
+import { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Home from "../pages/Home";
+import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
 
 const Private = ({ Item }) => {
   const { signed } = useAuth();
@@ -17,9 +17,10 @@ const RoutesApp = () => {
     <BrowserRouter>
       <Fragment>
         <Routes>
-          <Route exact path='/home' element={<Private Item={Home} />} />
-
-          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path="/home" element={<Private Item={Home} />} />
+          <Route path="/" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route path="*" element={<Signin />} />
         </Routes>
       </Fragment>
     </BrowserRouter>

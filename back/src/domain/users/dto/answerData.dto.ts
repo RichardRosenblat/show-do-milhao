222e9eee33d0b808b785c80b3ef5b3e3db1ca6fc
Richard Rosenblat <richard.rosenblat@linainfratech.com.br>
2023-01-19 11:59:02 -0300

    fix path error

--- back/src/domain/users/dto/answerData.dto.ts
+++ back/src/domain/users/dto/answerData.dto.ts
@@ -2,7 +2,7 @@ import { ObjectId } from 'bson';
 import { Transform } from 'class-transformer';
 import { IsBoolean, IsNotEmpty, IsObject } from 'class-validator';
 import { isExistingQuestion } from '../../questions/validator/doesQuestionExists';
-import { transformStringToObjectId } from 'src/util/ObjectIdTranform';
+import { transformStringToObjectId } from '../../../util/ObjectIdTranform';
 
 export class AnswerDataDTO {
   @IsNotEmpty()
